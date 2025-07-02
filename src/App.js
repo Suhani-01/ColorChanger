770e5855103e31b096116ad4912a2cd9c7badcf4
Suhani-01 <suhaninegi124@gmail.com>
2025-07-02 19:05:15 +0530

    Page Created

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import './App.css';
 
 function App() {
   return (
-    <div>
-      
+    <div className='main-container'>
+      <button className='container-button'>Click Me</button>
+      <p className='container-counter'>Number of times the container clicked : 0</p>
     </div>
   );
 }
