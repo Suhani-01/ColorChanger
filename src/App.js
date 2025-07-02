705fbb19c675fbd73675deb0a4374c45688bdf1c
Suhani-01 <suhaninegi124@gmail.com>
2025-07-02 19:22:14 +0530

    Event Counter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
-
+import React,{useState} from 'react';
 function App() {
+  const [counter,setCounter]=useState(0);
+
+  function containerClicked(){
+    setCounter(counter+1);
+  }
+  
   return (
-    <div className='main-container'>
+    <div className='main-container' onClick={containerClicked}>
       <button className='container-button'>Click Me</button>
-      <p className='container-counter'>Number of times the container clicked : 0</p>
+      <p className='container-counter'>Number of times the container clicked : {counter}</p>
     </div>
   );
 }
